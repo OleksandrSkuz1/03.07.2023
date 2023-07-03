5e732245f17ef84add139be7de48d74a46181a7d
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-03 19:39:00 +0300

    add

---
+++ main.py
@@ -0,0 +1,37 @@
+if __name__ == '__main__':
+
+
+    # # Primitive Date
+    # a = 1
+    # b = a
+    # b += 1
+    # print(a, b)
+    #
+    # # Complex Date
+    # c = [1]
+    # d = c
+    # d[0] += 1
+    # print(c, d)
+    #
+    # # Complex Date
+    # c = [1]
+    # d = c.copy()
+    # d[0] += 1
+    # print(c, d)
+
+    users = [
+        {"name" : "John",
+         "age" : 31
+        },
+        {"name" : "Jane",
+         "age" : 27
+        },
+        {"name": "Jack",
+         "age": 15
+         },
+        {"name": "Jack",
+         "age": 15
+         },
+    ]
+   temp_age
+
